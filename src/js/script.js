1c915e968ae9d235c7f69dfdc58d7f2a1ec00e3e
Karina-98 <115569415+Karina-98@users.noreply.github.com>
2025-06-14 16:46:47 +0200

    fead: learning_slider

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,31 @@
 const body = document.querySelector('body');
+
+const swiper = new Swiper('.swiper', {
+	loop: true,
+
+	// Navigation arrows
+	navigation: {
+		nextEl: '.learningSliderNext',
+		prevEl: '.learningSliderPrev',
+	},
+
+	pagination: {
+		el: '.learningSliderPrevPag',
+		clickable: true,
+	},
+	breakpoints: {
+		640: {
+			slidesPerView: 1,
+		},
+
+		768: {
+			slidesPerView: 2,
+			spaceBetween: 30,
+		},
+
+		1024: {
+			slidesPerView: 3,
+			spaceBetween: 40,
+		},
+	},
+});
